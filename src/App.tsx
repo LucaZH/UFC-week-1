41bea7769d6897fccd6baeb4f054af5222c03934
lucaZH <lucarandria1@gmail.com>
2024-05-20 09:29:44 +0300

    Refactor App component to include new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import "./App.css";
+import Branding from "./components/Branding";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="bg-gradient-to-b from-violet-300 to-pink-200 p-10 pt-5 transition-opacity md:p-20 md:pt-5">
+        <NavBar />
+        <Hero />
+      </div>
+      <Branding />
+      <div className="p-20 pt-10 md:p-20 md:pt-10">
+        <Features />
+      </div>
+    </>
+  );
 }
 export default App;
