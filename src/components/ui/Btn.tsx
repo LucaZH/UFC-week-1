23baa9614a8a70aaa72b3859a60ced1cd7ae587f
lucaZH <lucarandria1@gmail.com>
2024-05-20 09:31:59 +0300

    Modif class of bnt

--- src/components/ui/Btn.tsx
+++ src/components/ui/Btn.tsx
@@ -4,8 +4,10 @@ export function Btn({ active, text }: ButtonProps) {
   return (
     <button
       className={`rounded-3xl ${
-        active ? "bg-transparent border border-white" : "bg-white border"
-      } text-black w-28 h-11 text-center`}
+        active
+          ? "bg-transparent border border-white hover:bg-white hover:text-black"
+          : "bg-white border"
+      } text-black w-28 h-11 text-center hover:bg-transparent hover:text-white hover:scale-110 transition-all`}
     >
       {text}
     </button>
