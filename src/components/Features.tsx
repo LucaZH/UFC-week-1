2b7231fe9194743b1a4e853c1e526aead6351eee
lucaZH <lucarandria1@gmail.com>
2024-05-21 12:00:16 +0300

    Add features component

---
+++ src/components/Features.tsx
@@ -0,0 +1,41 @@
+import HText from "./ui/HText";
+
+function Features() {
+  return (
+    <section>
+      <div className="text-center">
+        <HText text="How It Works" />
+        <p className="text-gray-500">
+          Our drones capture moments with a bird's eye view
+        </p>
+      </div>
+      <div className="mt-16 md:flex md:justify-center gap-20">
+        <div className="flex flex-col items-center text-center">
+          <img src="/vite.svg" alt="" className="mb-10 w-1/3 h-1/3" />
+          <h4 className="text-black font-semibold text-xl">Propellers</h4>
+          <p className="text-gray-500 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex
+            blanditiis amet similique adipisci pariatur
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img src="/vite.svg" alt="" className="mb-10 w-1/3 h-1/3" />
+          <h4 className="text-black font-semibold text-xl">GPS</h4>
+          <p className="text-gray-500 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex
+            blanditiis amet similique adipisci pariatur
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img src="/vite.svg" alt="" className="mb-10 w-1/3 h-1/3" />
+          <h4 className="text-black font-semibold text-xl">Gyroscope</h4>
+          <p className="text-gray-500 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex
+            blanditiis amet similique adipisci pariatur
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Features;
