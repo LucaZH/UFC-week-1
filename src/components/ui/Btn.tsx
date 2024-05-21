cd4f2e9e7790a7a2a7ba7ee746379a38c15cc01e
lucaZH <lucarandria1@gmail.com>
2024-05-21 14:08:48 +0300

    Fix hover

--- src/components/ui/Btn.tsx
+++ src/components/ui/Btn.tsx
@@ -3,11 +3,11 @@ import ButtonProps from "../../types/Button";
 export function Btn({ active, text }: ButtonProps) {
   return (
     <button
-      className={`rounded-3xl ${
+      className={`rounded-3xl w-28 h-11 text-center border border-white text-black hover:scale-110 transition-all ${
         active
-          ? "bg-transparent border border-white hover:bg-white hover:text-black"
-          : "bg-white border"
-      } text-black w-28 h-11 text-center hover:bg-transparent hover:text-white hover:scale-110 transition-all`}
+          ? "bg-transparent  hover:bg-white hover:text-black"
+          : "bg-white hover:border-white hover:text-white hover:bg-transparent"
+      }`}
     >
       {text}
     </button>
