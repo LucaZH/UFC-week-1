ba9bcf5bcd88c8290235abd7320aa550808101e1
lucaZH <lucarandria1@gmail.com>
2024-05-23 10:18:11 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BlogNew from "./components/BlogNew";
 import Branding from "./components/Branding";
 import Caract from "./components/Caract";
 import Empowering from "./components/Empowering";
@@ -5,20 +6,23 @@ import Features from "./components/Features";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Product from "./components/Product";
+import Testmonial from "./components/Testmonial";
 
 function App() {
   return (
     <>
-      <div className="bg-gradient-to-b from-violet-300 to-pink-200 p-10 pt-5 transition-opacity md:p-20 md:pt-5">
+      <div className="bg-[url('/img/hero-bg.png')] bg-no-repeat bg-cover p-10 pt-5 pb-0 md:p-20 md:pt-5">
         <NavBar />
         <Hero />
+        <Branding />
       </div>
-      <Branding />
       <div className="flex flex-col p-10 gap-16 md:p-20 md:pt-10">
         <Features />
         <Empowering />
         <Caract />
         <Product />
+        <Testmonial />
+        <BlogNew />
       </div>
     </>
   );
