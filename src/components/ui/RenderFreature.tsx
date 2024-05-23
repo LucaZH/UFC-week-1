b8ae78e5e11786b38e3297e2a23dd0c1d5808378
lucaZH <lucarandria1@gmail.com>
2024-05-23 10:19:18 +0300

    Update text color

--- src/components/ui/RenderFreature.tsx
+++ src/components/ui/RenderFreature.tsx
@@ -9,7 +9,7 @@ function RenderFeature({
     <div className="relative p-4 border-t border-r border-b border-gray-200 rounded-r-3xl">
       <div className="pl-6 space-y-2">
         <li className="text-5xl font-semibold text-fuchsia-600">
-          <h3 className="text-2xl text-black">{title}</h3>
+          <h3 className="text-2xl text-zinc-700">{title}</h3>
         </li>
         <p className="text-gray-600">{description}</p>
       </div>
