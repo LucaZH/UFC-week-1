83aa9336ea364df575aaadc2d831b6b178f1700c
lucaZH <lucarandria1@gmail.com>
2024-05-23 10:18:45 +0300

    Add testmonial card component

---
+++ src/components/ui/TestmonialCard.tsx
@@ -0,0 +1,21 @@
+function TestmonialCad() {
+  return (
+    <div className="flex bg-pink-50 p-6 rounded-xl gap-10 w-7/12">
+      <img
+        src="/public/img/capadrone.webp"
+        alt="Tesmonial image"
+        className="w-72 h-auto rounded-xl object-cover"
+      />
+      <div className="flex flex-col justify-center">
+        <p className="text-xl font-extralight mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          laboriosam nesciunt officiis labore, sequi natus qui excepturi vitae
+          porro nam corrupti odio.
+        </p>
+        <h3 className="text-lg font-semibold">Mohamed</h3>
+        <span className="text-gray-400">UX/Design</span>
+      </div>
+    </div>
+  );
+}
+export default TestmonialCad;
