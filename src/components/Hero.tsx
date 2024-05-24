038de2ca50159f2a9dd7c0221ebac914f987a2a4
lucaZH <lucarandria1@gmail.com>
2024-05-24 15:10:49 +0300

    Fix hero mobile view

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,11 @@ import "../assets/styles/play.sass";
 function Hero() {
   return (
     <section className="md:flex h-full mt-20 mb-10">
-      <div className="flex flex-col content-center gap-8">
-        <h1 className="text-white text-4xl font-black w-4/5 md:text-7xl">
+      <div className="md:flex flex-col content-center gap-8">
+        <h1 className="text-white text-4xl font-black w-full md:w-4/5 md:text-7xl">
           The Best View In The World From Your Site
         </h1>
-        <p className="text-white w-4/5">
+        <p className="text-white w-full md:w-4/5">
           Provides you the lastest online learning system and material that help
           your knowledge growing
         </p>
@@ -22,7 +22,7 @@ function Hero() {
       <img
         src="/img/Phantom 4 pro.webp"
         alt="Phantom 4 pro"
-        className="w-auto h-96 scale-125"
+        className="w-auto h-96 scale-125 mt-5 md:mt-0 lg:mt-0"
       />
     </section>
   );
