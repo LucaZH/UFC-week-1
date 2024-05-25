17e276d4e0debdb9cc5d799b6d42c63f2ee14387
lucaZH <lucarandria1@gmail.com>
2024-05-25 09:56:17 +0300

    add the bradent to the border

--- src/components/ui/RenderFreature.tsx
+++ src/components/ui/RenderFreature.tsx
@@ -6,7 +6,7 @@ function RenderFeature({
   description: string;
 }) {
   return (
-    <div className="relative p-[1px] rounded-r-3xl bg-gradient-to-r from-white to-gray-100">
+    <div className="relative p-[1px] rounded-r-3xl bg-gradient-to-r from-white from-30% to-gray-300">
       <div className="rounded-r-3xl bg-white p-5">
         <div className="pl-6 space-y-2">
           <h3 className="text-2xl font-semibold text-zinc-700 before-dot">
